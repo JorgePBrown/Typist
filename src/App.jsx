5b98aac3361170e76fbcaebdd02f55df6e9861b6
Jorge Brown <j.brown@moovit-sp.com>
2023-12-20 17:17:30 +0000

    Add mode selection in the home screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import { createResource, createSignal } from "solid-js";
+import { Match, createResource, createSignal } from "solid-js";
 import "./App.css";
 import Word from "./components/Word";
 
+const Mode = {
+  RANDOM: 0,
+};
+
 function App() {
+  const [mode, setMode] = createSignal(undefined);
+
+  return (
+    <Switch>
+      <Match when={mode() === undefined}>
+        <button onClick={() => setMode(Mode.RANDOM)}>Random</button>
+      </Match>
+      <Match when={mode() === Mode.RANDOM}>
+        <button onClick={() => setMode(undefined)}>Back</button>
+        <RandomWords />
+      </Match>
+    </Switch>
+  );
+}
+
+function RandomWords() {
   const [words] = createResource(() =>
     fetch("./common-words.json").then((res) => res.json()),
   );
