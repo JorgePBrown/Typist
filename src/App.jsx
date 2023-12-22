f5b721e9b64455f66f477fc41e793aaea40708db
Jorge Brown <j.brown@moovit-sp.com>
2023-12-22 20:46:59 +0000

    Add Quotes mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import { Match, createResource, createSignal } from "solid-js";
+import { Match, createEffect, createResource, createSignal } from "solid-js";
 import "./App.css";
 import Word from "./components/Word";
+import TypedArea from "./components/TypedArea";
+import WordCounter from "./components/WordCounter";
 
 const Mode = {
   RANDOM: 0,
+  QUOTES: 1,
 };
 
 function App() {
@@ -13,10 +16,15 @@ function App() {
     <Switch>
       <Match when={mode() === undefined}>
         <button onClick={() => setMode(Mode.RANDOM)}>Random</button>
+        <button onClick={() => setMode(Mode.QUOTES)}>Quotes</button>
       </Match>
       <Match when={mode() === Mode.RANDOM}>
         <button onClick={() => setMode(undefined)}>Back</button>
         <RandomWords />
+      </Match>
+      <Match when={mode() === Mode.QUOTES}>
+        <button onClick={() => setMode(undefined)}>Back</button>
+        <Quotes />
       </Match>
     </Switch>
   );
@@ -30,6 +38,22 @@ function RandomWords() {
   return (
     <Show when={!words.loading} fallback={<div>Loading...</div>}>
       <Word words={words()} />
+    </Show>
+  );
+}
+
+function Quotes() {
+  const [quote, { refetch }] = createResource(() =>
+    fetch("https://api.api-ninjas.com/v1/quotes", { headers: { "X-Api-Key": import.meta.env.VITE_API_NINJAS_API_KEY }}).then((res) => res.json()),
+  );
+
+  document.addEventListener("wordtyped", refetch)
+
+  return (
+    <Show when={!quote.loading} fallback={<div>Loading...</div>}>
+      <WordCounter/>
+      <TypedArea word={quote()[0].quote}/>
+      <p>Author: {quote()[0].author}</p>
     </Show>
   );
 }
